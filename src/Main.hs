dff0d9e35d4ea270f1bf439d77ea79a123d664ac
Chris Done <chrisdone@gmail.com>
2013-07-02 15:07:32 +0200

    Start of suave panel.

--- src/Main.hs
+++ src/Main.hs
@@ -1,32 +1,37 @@
+{-# OPTIONS -Wall #-}
+
 -- | Chris Done's XMonad program.
 
 module Main where
 
+import           Control.Concurrent
+import           Control.Monad
 import qualified Data.Map                  as M
 import           XMonad
 import           XMonad.Actions.DeManage   (demanage)
 import           XMonad.Config.Gnome       (gnomeConfig)
 import           XMonad.Hooks.EwmhDesktops (ewmh)
 import           XMonad.Hooks.FadeInactive (fadeInactiveLogHook)
+import           XMonad.Suave
 import           XMonad.Util.Run           (spawnPipe)
 
 -- | Main entry point.
 main :: IO ()
 main = do
-  _ <- spawnPipe "xcompmgr -t-5 -l-5 -r4.2 -o.55 -F"
-  xmonad $ ewmh gnomeConfig
-           { terminal          = "gnome-terminal"
-           , modMask           = mod4Mask
-           , focusFollowsMouse = False
-           , borderWidth       = 0
-           , logHook           = fadeInactiveLogHook 0xbbbbbbbb
-           , keys              = newKeys
-           , manageHook        = myManageHook
-           }
-  where newKeys x = M.union (keys defaultConfig x) (M.fromList (myKeys x))  
-        myKeys conf@(XConfig{modMask=modm}) =
+  suave <- suaveStart
+  void (spawnPipe "xcompmgr -t-5 -l-5 -r4.2 -o.55 -F")
+  void (forkIO (xmonad (ewmh gnomeConfig
+                             { terminal          = "gnome-terminal"
+                             , modMask           = mod4Mask
+                             , focusFollowsMouse = False
+                             , borderWidth       = 0
+                             , logHook           = fadeInactiveLogHook 0xbbbbbbbb
+                             , keys              = newKeys
+                             , manageHook        = suaveManageHook
+                             , layoutHook        = suaveLayout
+                             , startupHook       = suaveStartupHook suave
+                             })))
+  suaveMain
+  where newKeys x = M.union (keys defaultConfig x) (M.fromList (myKeys x))
+        myKeys (XConfig{modMask=modm}) =
           [((modm,xK_d),withFocused demanage)]
-        myManageHook = composeAll
-         [ className =? "Unity-2d-panel"    --> doIgnore
-         , className =? "Unity-2d-launcher" --> doIgnore
-         ]

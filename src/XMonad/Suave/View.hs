6196b9abce3c03cbd762dd3e891528ef0a539551
Chris Done <chrisdone@gmail.com>
2015-02-20 15:21:42 +0100

    hindent reformat

--- src/XMonad/Suave/View.hs
+++ src/XMonad/Suave/View.hs
@@ -14,75 +14,82 @@ import Text.Blaze.Internal
 
 -- | View for the panel.
 page :: Html
-page = do
-  docType
-  html $ do
-    head $ do
-      theme
-    body $ do
-      rhs
+page =
+  do docType
+     html $
+       do head $
+            do theme
+          body $
+            do rhs
 
 -- | CSS theme.
 theme :: Html
-theme = do
-  link ! href "http://netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.css"
-       ! rel "stylesheet"
-  style $ mconcat ["body {"
-                  ,"font-size: 22px;"
-                  ,"font-family: ubuntu;"
-                  ,"background: #ff0000;"
-                  ,"color: #bbbbbb;"
-                  ,"height: 40px;"
-                  ,"overflow: hidden;"
-                  ,"padding: 0.3em;"
-                  ,"margin: 0;"
-                  ,"text-shadow: -0.08em -0.08em 0.01em #292929;"
-                  ,"background: -webkit-linear-gradient(top, #444, #333) no-repeat #333;"
-                  ,"-webkit-touch-callout: none;"
-                  ,"-webkit-user-select: none;"
-                  ,"-khtml-user-select: none;"
-                  ,"-moz-user-select: none;"
-                  ,"-ms-user-select: none;"
-                  ,"user-select: none;"
-                  ,"cursor: default;"
-                  ,"}"
-                  ,".indicator {"
-                  ,"margin-right: 0.5em; visibility:visible"
-                  ,"}"
-                  ,".ip { display: none }"
-                  ,"#wifi:hover .ip { display: inline }"
-                  ,"#date {"
-                  ,"}"
-                  ,"#rhs {"
-                  ,"float: right;"
-                  ,"}"
-                  ,"#center {"
-                  ,"text-align:center; position: absolute; top:0;left:0;bottom:0;right:0; line-height: 40px"
-                  ,"}"
-                  ,"#lhs,#rhs{"
-                  ,"visibility:hidden"
-                  ,"}"
-                  ,"#power-off {"
-                  ,"cursor: pointer;"
-                  ,"}"
-                  ,"#power-off:hover {"
-                  ,"color: #fff;"
-                  ,"}"]
+theme =
+  do link !
+       href "http://netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.css" !
+       rel "stylesheet"
+     style $
+       mconcat ["body {"
+               ,"font-size: 22px;"
+               ,"font-family: ubuntu;"
+               ,"background: #ff0000;"
+               ,"color: #bbbbbb;"
+               ,"height: 40px;"
+               ,"overflow: hidden;"
+               ,"padding: 0.3em;"
+               ,"margin: 0;"
+               ,"text-shadow: -0.08em -0.08em 0.01em #292929;"
+               ,"background: -webkit-linear-gradient(top, #444, #333) no-repeat #333;"
+               ,"-webkit-touch-callout: none;"
+               ,"-webkit-user-select: none;"
+               ,"-khtml-user-select: none;"
+               ,"-moz-user-select: none;"
+               ,"-ms-user-select: none;"
+               ,"user-select: none;"
+               ,"cursor: default;"
+               ,"}"
+               ,".indicator {"
+               ,"margin-right: 0.5em; visibility:visible"
+               ,"}"
+               ,".ip { display: none }"
+               ,"#wifi:hover .ip { display: inline }"
+               ,"#date {"
+               ,"}"
+               ,"#rhs {"
+               ,"float: right;"
+               ,"}"
+               ,"#center {"
+               ,"text-align:center; position: absolute; top:0;left:0;bottom:0;right:0; line-height: 40px"
+               ,"}"
+               ,"#lhs,#rhs{"
+               ,"visibility:hidden"
+               ,"}"
+               ,"#power-off {"
+               ,"cursor: pointer;"
+               ,"}"
+               ,"#power-off:hover {"
+               ,"color: #fff;"
+               ,"}"]
 
 -- | Right-hand size.
 rhs :: Html
-rhs = do
-  span !# "lhs" $ do
-    span !# "i3" $ return ()
-  span !# "center" $ do
-    span !# "clockin" $ return ()
-  span !# "rhs" $ do
-    span !. "indicator" !# "date" $ return ()
+rhs =
+  do span !# "lhs" $
+       do span !# "i3" $
+            return ()
+     span !# "center" $
+       do span !# "clockin" $
+            return ()
+     span !# "rhs" $
+       do span !. "indicator" !# "date" $
+            return ()
 
 -- | Class names.
-(!.) :: Attributable h => h -> AttributeValue -> h
+(!.) :: Attributable h
+     => h -> AttributeValue -> h
 span !. value = span ! class_ value
 
 -- | Class names.
-(!#) :: Attributable h => h -> AttributeValue -> h
+(!#) :: Attributable h
+     => h -> AttributeValue -> h
 span !# value = span ! id value
--- src/XMonad/Suave/Window.hs
+++ src/XMonad/Suave/Window.hs
@@ -26,45 +26,56 @@ import           Text.Blaze.Html.Renderer.Text
 
 -- | Start up a Suave panel.
 suaveStart :: IO (Suave)
-suaveStart = do
-  void initGUI
-  window <- windowNew
-  vContainer <- vBoxNew False 0
-  scrolledWindow <- scrolledWindowNew Nothing Nothing
-  scrolledWindowSetPolicy scrolledWindow PolicyNever PolicyNever
-  webview <- webViewNew
-  set window [containerChild := vContainer
-             ,windowTitle    := suaveWindowTitle]
-  boxPackStart vContainer scrolledWindow PackGrow 0
-  set scrolledWindow [containerChild := webview]
-  Just document <- webViewGetDomDocument webview
-  Just body <- documentGetBody document
-  htmlElementSetInnerHTML body (unpack (renderHtml page))
-  Just i3 <- fmap (fmap castToHTMLElement)
-                  (documentGetElementById document "i3")
-  Just date <- fmap (fmap castToHTMLElement)
-                    (documentGetElementById document "date")
-  Just clockin <- fmap (fmap castToHTMLElement)
-                       (documentGetElementById document "clockin")
-  void (forkIO (fix (\loop -> do postGUISync (void (try (updateUI i3 date clockin) :: IO (Either SomeException ())))
-                                 threadDelay (1000 * 1000)
-                                 loop)))
-  void (onDestroy window mainQuit)
-  void (widgetShowAll window)
-  return (Suave window)
+suaveStart =
+  do void initGUI
+     window <- windowNew
+     vContainer <- vBoxNew False 0
+     scrolledWindow <-
+       scrolledWindowNew Nothing Nothing
+     scrolledWindowSetPolicy scrolledWindow PolicyNever PolicyNever
+     webview <- webViewNew
+     set window [containerChild := vContainer,windowTitle := suaveWindowTitle]
+     boxPackStart vContainer scrolledWindow PackGrow 0
+     set scrolledWindow [containerChild := webview]
+     Just document <- webViewGetDomDocument webview
+     Just body <- documentGetBody document
+     htmlElementSetInnerHTML body
+                             (unpack (renderHtml page))
+     Just i3 <-
+       fmap (fmap castToHTMLElement)
+            (documentGetElementById document "i3")
+     Just date <-
+       fmap (fmap castToHTMLElement)
+            (documentGetElementById document "date")
+     Just clockin <-
+       fmap (fmap castToHTMLElement)
+            (documentGetElementById document "clockin")
+     void (forkIO (fix (\loop ->
+                          do postGUISync (void (try (updateUI i3 date clockin) :: IO (Either SomeException ())))
+                             threadDelay (1000 * 1000)
+                             loop)))
+     void (onDestroy window mainQuit)
+     void (widgetShowAll window)
+     return (Suave window)
 
 -- | Update the contents of the panel.
 updateUI :: HTMLElement -> HTMLElement -> HTMLElement -> IO ()
 updateUI i3 date clockin =
   do status <- i3status
-     htmlElementSetInnerHTML i3 (unpack status)
+     htmlElementSetInnerHTML i3
+                             (unpack status)
      now <- getZonedTime
-     htmlElementSetInnerHTML date (formatTime defaultTimeLocale "%A %F %T %z (%Z)" now)
+     htmlElementSetInnerHTML date
+                             (formatTime defaultTimeLocale "%A %F %T %z (%Z)" now)
      config <- getClockinConfig
      entries <- readClockinEntries config
-     now <- fmap zonedTimeToLocalTime getZonedTime
-     let desc = onelinerStatus now (clockinStatus config now entries)
-     htmlElementSetInnerHTML clockin ("<i class='fa fa-clock-o'></i> " ++ T.unpack desc)
+     now <-
+       fmap zonedTimeToLocalTime getZonedTime
+     let desc =
+           onelinerStatus now
+                          (clockinStatus config now entries)
+     htmlElementSetInnerHTML clockin
+                             ("<i class='fa fa-clock-o'></i> " ++ T.unpack desc)
   where readInt :: Text -> Int
         readInt = read . unpack
 
@@ -72,7 +83,8 @@ updateUI i3 date clockin =
 i3status :: IO Text
 i3status =
   do fp <- getDataFileName "i3status.conf"
-     (_in,out,_err,pid) <- runInteractiveCommand ("i3status -c " ++ show fp)
+     (_in,out,_err,pid) <-
+       runInteractiveCommand ("i3status -c " ++ show fp)
      line <- T.hGetLine out
      terminateProcess pid
      return line
